35861630528f4084af7c832403f2edeae72a4097
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2021-11-07 18:21:31 +0100

    step 1
    add :
    -navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <ul>
+                <NavLink to="/" exact className="hover"
+                    activeClassName="nav-active">
+                    <li>accueil</li>
+                </NavLink>
+
+                <li className="nav-portfolio">portfolio
+                    <ul className="nav-projects">
+                        <NavLink to="/project-1" activeClassName="nac-active"
+                            className="hover">
+                                <li>projet 1</li>
+                        </NavLink>
+                        <NavLink to="/project-2" activeClassName="nac-active"
+                            className="hover">
+                                <li>projet 2</li>
+                        </NavLink>
+                        <NavLink to="/project-3" activeClassName="nac-active"
+                            className="hover">
+                                <li>projet 3</li>
+                        </NavLink>
+                        <NavLink to="/project-4" activeClassName="nac-active"
+                            className="hover">
+                                <li>projet 4</li>
+                        </NavLink>
+                    </ul>    
+                </li>
+
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
---
+++ src/pages/Projects.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export const Project1 = () => {
+    return (
+        <main>
+            <div className="project">
+                project 1
+            </div>
+        </main>
+    );
+};
+export const Project2 = () => {
+    return (
+        <main>
+            <div className="project">
+                project 2
+            </div>
+        </main>
+    );
+};
+export const Project3 = () => {
+    return (
+        <main>
+            <div className="project">
+                project 3
+            </div>
+        </main>
+    );
+};
+export const Project4 = () => {
+    return (
+        <main>
+            <div className="project">
+                project 4
+            </div>
+        </main>
+    );
+};
+
