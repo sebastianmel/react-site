d7a510a58493d1f08092bc04244488582b07fbd6
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2021-12-02 16:08:54 +0100

    add
    creation of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import DynamicText from '../components/DynamicText';
 import Navigation from '../components/Navigation';
 import SocialNetwork from '../components/SocialNetwork';
 
 const Home = () => {
     return (
         <div>
-            <Navigation/>
-            <SocialNetwork/>
+            <div className="home">
+                <Navigation />
+                <SocialNetwork />
+                <div className="home-main">
+                    <div className="min-content">
+                    <h1>FS AGENCY</h1>
+                    <h2>
+                        <DynamicText />
+                    </h2>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
