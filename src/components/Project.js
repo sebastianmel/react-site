636803c36264eb096529d6fd2cbbc5e63d1dcc97
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2021-12-05 23:33:01 +0100

    add
    projects pages add contents

--- src/components/Project.js
+++ src/components/Project.js
@@ -18,6 +18,21 @@ const Project = (props) => {
                 </ul>
 
             </div>
+            <div className="img-content">
+                <div className="img-container hover">
+                    <span>
+                        <h3>{project.title}</h3>
+                        <p>{project.infos}</p>
+                    </span>
+                    <img  src={project.img} alt={project.title} />
+                </div>
+                <div className="button-container">
+                    <a href={project.link} target="blank" rel="noopener"
+                    className="hover">
+                        <span className="button">voir le site</span>
+                    </a>
+                </div>
+            </div>
             
         </div>
     );
